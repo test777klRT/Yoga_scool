ac7138865864eb010ef76f5add20bb88dd08cb75
Stanislav Fedenev <103477649+test777klRT@users.noreply.github.com>
2022-08-31 20:13:30 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
 //$('body').hide()
+
+
 function imgSlider(anything){
  document.querySelector('.img-content').src = anything;
 }
@@ -15,3 +17,20 @@ function changeText(num){
  if (num === 3)
  document.getElementById('simple').innerHTML = "Not just meditation,<br> it's your <span>harmony</span> with yourself";
 }
+
+
+
+MicroModal.init({
+ onShow: modal => console.info(`${modal.id} is shown`), // [1]
+ onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+ openTrigger: 'data-custom-open', // [3]
+ closeTrigger: 'data-custom-close', // [4]
+ openClass: 'is-open', // [5]
+ disableScroll: true, // [6]
+ disableFocus: false, // [7]
+ awaitOpenAnimation: false, // [8]
+ awaitCloseAnimation: false, // [9]
+ debugMode: true // [10]
+});
+
+document.getElementById('dataToday').valueAsDate= new Date();
